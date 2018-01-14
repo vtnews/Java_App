85a1ec824d67ded3d34d40630e2f8298b32a4868
root <some@qq.com>
2018-01-15 00:56:24 +0800

    一点点说明

--- src/main/java/com/mufeng/finaldemo/FinalReferenceEscapeExample.java
+++ src/main/java/com/mufeng/finaldemo/FinalReferenceEscapeExample.java
@@ -7,11 +7,22 @@ public class FinalReferenceEscapeExample {
     public FinalReferenceEscapeExample () {
         i = 1; // 1写final域
         obj = this; // 2 this引用在此"逸出"
+        /*
+         * 首先，1和2无依赖关系，可能会重排序。
+         * 2 h-b 3
+         * 3 h-b 4
+         *
+         * 在初始化开辟空间时，2先执行，则obj这个普通变量已经被赋予了this值，确定指向的地址空间。而1的final还未初始化
+         * 则变成：
+         * ThreadB:    -> 3 -> 4 -
+         * ThreadA: 2 -           -> 1
+         * 则4中读取的，是还未执行1前的final变量i值。
+         */
     }
 
     public static void writer() {
         new FinalReferenceEscapeExample ();
-//        obj = new FinalReferenceEscapeExample (); //应该将逸出的去掉，在这实例化
+//        obj = new FinalReferenceEscapeExample (); //应该将逸出的去掉，在这赋值
     }
 
     public static void reader() {
